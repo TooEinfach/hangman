83c81d367a58e97a0a49063717510b67ff5edef3
Nick <whiteshadow148@gmail.com>
2020-12-01 14:37:32 -0700

    Created function to validate user input is a letter
    Also started letter in word function

--- hangman.py
+++ hangman.py
@@ -3,6 +3,7 @@ import random
 words = ["keyboard", "python", "owen", "blue", "tyrannosaurus"]
 random_word = random.choice(words)
 blanks = []
+letter = input("Guess a letter: ")
 
 def rand_word(self): # Funcion to count the number of letters in random word
     count = 0
@@ -24,9 +25,30 @@ def word_blanks(self): # Function to print out blanks for the random word.
 
     return word_blanks
 
+def validate_input(letter): # Check to see if the user input is a letter
+    while True:
+        if letter.isalpha():
+            return True
+        else:
+            input("Please enter a letter: ")
+            return False
+
+def letter_in_word(letter, random_word): # Function to check is Users letter is in word
+    word = []
+    
+    for l in random_word:
+        word.append(l)
+    
+    if letter in random_word:
+        print("You guessed right")
+    else:
+        print("Guess again")
+
 
 rand_word(rand_word)
 word_blanks(word_blanks)
+validate_input(letter)
+letter_in_word(letter, random_word)
 
 
 
