a590c42d641e7b896c8d78da991e681229fb11a1
Nick <whiteshadow148@gmail.com>
2020-12-07 16:49:27 -0700

    Cleaning up bugs

--- hangman.py
+++ hangman.py
@@ -40,81 +40,95 @@ def validate_input(letter): # Check to see if the user input is a letter
             letter = input("Please enter a letter: ")
             return letter
             
-def guess_word(random_word, guess_count):
+def guess_word(random_word, guess_count, round_word):
     user_guess = input("Guess your word: ")
 
-    while True:
-        if user_guess == random_word:
-            print("YOU GOT IT YOU WIN!!")
-            print()
-            return            
-        else:
-            print("Sorry that guess was wrong")
-            print()
-            guess_count += 1
-            continue
+    # while True:
+    if user_guess == random_word:
+        print("YOU GOT IT YOU WIN!!")
+        print()
+        return            
+    else:
+        print("Sorry that guess was wrong")
+        print()
+        # guess_count += 1
+        # print(guess_count)
+        # return guess_count
+        playgame(random_word, round_word, guess_count)
 
-def letter_in_word(letter, random_word, guess_count): # Function to check is Users letter is in word
+def letter_in_word(letter, random_word, guess_count,round_word): # Function to check is Users letter is in word
     word = []
     print(letter)
 
     for l in random_word:
         word.append(l)
     
-    while guess_count <= 15:
-        letter = validate_input(letter) # had to do this so that when number put in the vaildate letter when returned set for the rest of the function.
+    # while guess_count <= 15:
+    #     letter = validate_input(letter) # had to do this so that when number put in the vaildate letter when returned set for the rest of the function.
         
-        if letter in word:
-            print("There is a/an " + letter)
-            return letter
-            
-        else:
-            print("There is no " + letter)
-            continue             
+    if letter in word:
+        print("There is a/an " + letter)
+        return letter
+        playgame(rand_word, round_word, guess_count)
+        
+    else:
+        print("There is no " + letter)
+        playgame(rand_word, round_word, guess_count)           
 
             # print(guess_count)
-        guess_count += 1
+    guess_count += 1
     
-    if guess_count >= 15:
-        print("Sorry you lose please play again the word was " + random_word)
+    
 
-def playgame(rand_word):
-    round_word = word_blanks(random_word)
-    print(random_word)
-    while True:
+def playgame(rand_word, round_word, guess_count):
+    guess_count += 1
+    print(guess_count)
+    while guess_count <= 15:
         menu()
         print("Your word is " + round_word)
         print(random_word)
         print()
-        choice = int(input("Select Your option:  "))
-
+        try:
+            choice = int(input("Select Your option:  "))
+        except ValueError:
+            print("Please select an number between 1 and 3")
+            print()
+            continue
+        
         if 1 <= choice <= 3:
             if choice == 1:
                 letter = input("Please guess a letter: ")
-                letter_in_word(letter, random_word, guess_count)
+                letter_in_word(letter, random_word, guess_count, round_word)
             elif choice == 2:
-                guess_word(random_word, guess_count)
+                guess_word(random_word, guess_count, round_word)
             elif choice == 3:
                 print("Please come back and play again")
                 break
         else:
             choice = input("Please enter a number between 1 and 3: ")
         return
+    return guess_count
 
 def main():
-    global random_word, blanks
+    global random_word, blanks, guess_count
     random_word = random.choice(words)
+    round_word = word_blanks(random_word)
     while True:
-        playgame(random_word)
+        playgame(random_word, round_word, guess_count)
+
+        if guess_count <= 15:
+            print("Please come back and play again.")
 
         play_again = input("Would you like to play again Y/N: ").lower()
         if play_again == "y":
             blanks = []
             random_word = random.choice(words)
-            playgame(random_word)
-        else:
+            main()
+            # playgame(random_word)
+        elif play_again == "n":
             print("Come back and play again")
             break
+        
         
     
 
@@ -129,4 +143,3 @@ if __name__ == "__main__":
 
 
 
-print(random_word)
